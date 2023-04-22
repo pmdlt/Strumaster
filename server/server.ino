588cad0733104bc2effd374825f65314b3a2e9a4
Paul Madelénat <dev@pmd.lt>
2023-04-22 13:23:03 +0200

    Add support multiple servo and steppers + debug stepper func

--- server/server.ino
+++ server/server.ino
@@ -1,9 +1,9 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <Servo.h>
-#include <Stepper.h>
+#include <AccelStepper.h>
 
-#define STEPS 200  // number of steps of steppers
+#define stepsPerSecond 200  // number of steps of steppers
 
 // Webserver config
 const char* ssid = "StrumMaster";
@@ -23,22 +23,26 @@ uint8_t servo1_pin = D4;
 
 // Servos init
 Servo servo1;
+Servo servo[7] = {
+  Servo(), servo1
+};
 
 // Steppers pin attribution
-uint8_t stp1_1 = D4;
-uint8_t stp1_2 = D4;
-uint8_t stp1_3 = D4;
-uint8_t stp1_4 = D4;
+uint8_t stp1_step = D4;
+uint8_t stp1_dir = D5;
 
 // Steppers init
-Stepper stepper1(STEPS, stp1_1, stp1_2, stp1_3, stp1_4);
+AccelStepper stp1(1, stp1_step, stp1_dir);
+AccelStepper stp[7] = {
+  AccelStepper(), stp1
+};
 
 void setup() {
   // Servos pin attachment
-  servo1.attach(servo1_pin);
+  servo[1].attach(servo1_pin);
 
   // Steppers speed init
-  stepper1.setSpeed(30);
+  stp[1].setMaxSpeed(stepsPerSecond);
 
   // WiFi initialisation
   WiFi.softAP(ssid, password);
@@ -46,42 +50,73 @@ void setup() {
   delay(100);
 
   // Commands handle
-  server.on("/connect", connect);
-  server.on("/play_note", play_note);
-  server.on("/stop", stop);
-  server.onNotFound(not_supported);
+  server.on("/connect", handleConnect);
+  server.on("/play_note", handlePlayNote);
+  server.on("/stop", handleStop);
+
+  server.on("/debug_stepper", HTTP_GET, []() {
+    handleDebugStepper();
+  });
+  server.onNotFound(handleNotSupported);
 
   server.begin();
 }
 void loop() {
   server.handleClient();
+  stp1.run();
 }
 
 ///////////////////////////////////////////////////////////
 
 void activate_stepper(int id, int position) {
-
 }
 
 void activate_servo(int id) {
-  servo.write(45);
+  servo1.write(45);
+  servo1.write(0);
 }
 
-void connect() {
+void handleConnect() {
   server.send(200, "text/plain", "Connexion successful. Try to play a note :) !");
 }
 
-void play_note() {
-  activate_stepper(1, 1);
+void handlePlayNote() {
+  activate_stepper(1, 0);
   activate_servo(1);
 
   server.send(200, "text/plain", "");
 }
 
-void stop() { 
+void handleStop() {
   server.send(200, "text/plain", "All motor off.");
 }
 
-void not_supported() {
+void handleNotSupported() {
   server.send(404, "text/plain", "Command not supported.");
+}
+
+void handleDebugStepper() {
+  int function = server.arg("function").toInt();
+  int id = server.arg("id").toInt();
+  int value = server.arg("value").toInt();
+  int rtnv = 0;
+
+  switch (function) {
+    case 1:
+      stp[id].moveTo(value);
+      break;
+    case 2:
+      stp[id].move(value);
+      break;
+    case 3:
+      rtnv = stp[id].currentPosition();
+      break;
+    case 4:
+      rtnv = stp[id].distanceToGo();
+      break;
+
+    default:
+      return;
+  }
+  server.send(200, "text/plain", "Executed. Return value : " + rtnv);
 }
