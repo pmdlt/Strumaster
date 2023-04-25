9fd123160455bdf0c5039060a0630428d113c3d4
Paul Madelénat <18498650+pmdlt@users.noreply.github.com>
2023-04-25 11:56:40 +0000

    Add DebugStepper vue

--- client/StrumMasterApp/src/router/index.js
+++ client/StrumMasterApp/src/router/index.js
@@ -16,6 +16,20 @@ const routes = [
       },
     ],
   },
+  {
+    path: '/',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '/debug_stepper',
+        name: 'Debug Stepper',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/DebugStepper.vue'),
+      },
+    ],
+  },
 ]
 
 const router = createRouter({
