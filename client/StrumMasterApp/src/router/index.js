b0683bd35d7b70b1bffcca61780107fdc237395d
Paul Madelénat <18498650+pmdlt@users.noreply.github.com>
2023-04-25 13:28:35 +0000

    Update router

--- client/StrumMasterApp/src/router/index.js
+++ client/StrumMasterApp/src/router/index.js
@@ -2,34 +2,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
-      },
-    ],
-  },
-  {
-    path: '/debug_stepper',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Debug Stepper',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/DebugStepper.vue'),
-      },
-    ],
-  },
+  { path: '/', component: () => import('@/views/Home.vue') },
+  { path: '/debug_stepper', component: () => import('@/views/DebugStepper.vue') },
 ]
 
 const router = createRouter({
