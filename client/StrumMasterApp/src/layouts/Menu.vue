23e41a59ee2716495d1d586fcde680abd3401f8b
Paul Madelénat <18498650+pmdlt@users.noreply.github.com>
2023-05-03 11:06:36 +0000

    Edit menu

---
+++ client/StrumMasterApp/src/layouts/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-container>
+
+        <v-btn block prepend-icon="mdi-stop-circle-outline" stacked variant="outlined" color="red" size="large">
+            Emergency Stop
+        </v-btn>
+        <br>
+        <v-btn block prepend-icon="mdi-pause" variant="outlined" color="black">
+            Pause
+        </v-btn>
+        <br>
+        <v-btn block prepend-icon="mdi-play" variant="outlined" color="green">
+            continue
+        </v-btn>
+        <br>
+        <v-btn block prepend-icon="mdi-update" stacked variant="outlined" color="orange" size="large">
+            Reset all motors
+        </v-btn>
+
+    </v-container>
+</template>
+  
--- client/StrumMasterApp/src/views/Song.vue
+++ client/StrumMasterApp/src/views/Song.vue
@@ -1,58 +1,40 @@
 <template>
     <v-app id="inspire">
-      <v-main class="bg-grey-lighten-3">
-        <v-container>
-          <v-row>
-            <v-col cols="12" sm="3">
-              <v-sheet rounded="lg">
-                <v-container>
-  
-                  <v-btn block prepend-icon="mdi-stop-circle-outline" stacked variant="outlined" color="red">
-                    Emergency Stop
-                  </v-btn>
-                  <br>
-                  <v-btn block prepend-icon="mdi-pause" variant="outlined" color="black">
-                    Pause
-                  </v-btn>
-                  <br>
-                  <v-btn block prepend-icon="mdi-play" variant="outlined" color="green">
-                    continue
-                  </v-btn>
-                  <br>
-                  <v-btn block prepend-icon="mdi-update" stacked variant="outlined" color="orange">
-                    Reset all motors
-                  </v-btn>
-  
-                </v-container>
-              </v-sheet>
-            </v-col>
-  
-            <v-col cols="12" sm="9">
-              <v-sheet min-height="70vh" rounded="lg">
-                <v-container>
-                  <v-form ref="form">
-                    <p class="text-center">Insert a midi file to be played by the guitar<br><br></p>
-                    <v-file-input v-model="file" label="MIDI File" variant="solo"></v-file-input>
-  
-                    <p class="text-center">Parameters</p>
-  
-                    <p class="text-left">MIDI Channel<br></p>
-                    <v-slider v-model="channel" :min="0" :max="16" :step="1" thumb-label></v-slider>
-  
-                    <div :class="text - h6">Speed<br></div>
-  
-                    <v-slider v-model="speed" :min="0" :max="100" thumb-label></v-slider>
-  
-                    <v-btn type="submit" block class="mt-2">Send and play</v-btn>
-                  </v-form>
-                </v-container>
-              </v-sheet>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
+        <v-main class="bg-grey-lighten-3">
+            <v-container>
+                <v-row>
+                    <v-col cols="12" sm="3">
+                        <v-sheet rounded="lg">
+                            <Menu />
+                        </v-sheet>
+                    </v-col>
+
+                    <v-col cols="12" sm="9">
+                        <v-sheet min-height="70vh" rounded="lg">
+                            <v-container>
+                                <v-form ref="form">
+                                    <p class="text-center">Insert a midi file to be played by the guitar<br><br></p>
+                                    <v-file-input v-model="file" label="MIDI File" variant="solo"></v-file-input>
+
+                                    <p class="text-center">Parameters</p>
+
+                                    <p class="text-left">MIDI Channel<br></p>
+                                    <v-slider v-model="channel" :min="0" :max="16" :step="1" thumb-label></v-slider>
+
+                                    <div :class="text - h6">Speed<br></div>
+
+                                    <v-slider v-model="speed" :min="0" :max="100" thumb-label></v-slider>
+
+                                    <v-btn type="submit" block class="mt-2">Send and play</v-btn>
+                                </v-form>
+                            </v-container>
+                        </v-sheet>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
     </v-app>
-  </template>
+</template>
 
 <script>
 export default {
@@ -77,15 +59,15 @@ export default {
             const url = `http://192.168.1.1/play_midi?${params.toString()}`;
             fetch(url, { method: "GET" })
                 .then(response => {
-                if (!response.ok) {
-                    throw new Error("Network response was not ok");
-                }
-                // handle successful response
-            })
+                    if (!response.ok) {
+                        throw new Error("Network response was not ok");
+                    }
+                    // handle successful response
+                })
                 .catch(error => {
-                console.error("There was a problem with the network request:", error);
-                // handle error
-            });
+                    console.error("There was a problem with the network request:", error);
+                    // handle error
+                });
         }
     }
 }
