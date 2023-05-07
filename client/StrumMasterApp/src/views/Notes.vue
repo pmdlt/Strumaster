ef4896ce8bda924a521bc4281c6cf9805f1a8c8f
Paul Madelénat <18498650+pmdlt@users.noreply.github.com>
2023-05-07 09:03:06 +0000

    Add playNote vue and script

--- client/StrumMasterApp/src/views/Notes.vue
+++ client/StrumMasterApp/src/views/Notes.vue
@@ -1,92 +1,63 @@
 <template>
-    <v-app id="inspire">
-      <v-main class="bg-grey-lighten-3">
-        <v-container>
-          <v-row>
-            <v-col cols="12" sm="3">
-              <v-sheet rounded="lg">
-                <v-container>
-  
-                  <v-btn block prepend-icon="mdi-stop-circle-outline" stacked variant="outlined" color="red">
-                    Emergency Stop
-                  </v-btn>
-                  <br>
-                  <v-btn block prepend-icon="mdi-pause" variant="outlined" color="black">
-                    Pause
-                  </v-btn>
-                  <br>
-                  <v-btn block prepend-icon="mdi-play" variant="outlined" color="green">
-                    continue
-                  </v-btn>
-                  <br>
-                  <v-btn block prepend-icon="mdi-update" stacked variant="outlined" color="orange">
-                    Reset all motors
-                  </v-btn>
-  
-                </v-container>
-              </v-sheet>
-            </v-col>
-  
-            <v-col cols="12" sm="9">
-              <v-sheet min-height="70vh" rounded="lg">
-                <v-container>
-                  <v-form ref="form">
-                    <p class="text-center">Insert a midi file to be played by the guitar<br><br></p>
-                    <v-file-input v-model="file" label="MIDI File" variant="solo"></v-file-input>
-  
-                    <p class="text-center">Parameters</p>
-  
-                    <p class="text-left">MIDI Channel<br></p>
-                    <v-slider v-model="channel" :min="0" :max="16" :step="1" thumb-label></v-slider>
-  
-                    <div :class="text - h6">Speed<br></div>
-  
-                    <v-slider v-model="speed" :min="0" :max="100" thumb-label></v-slider>
-  
-                    <v-btn type="submit" block class="mt-2">Send and play</v-btn>
-                  </v-form>
-                </v-container>
-              </v-sheet>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-    </v-app>
-  </template>
+  <v-app id="inspire">
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="3">
+            <v-sheet rounded="lg">
+              <the-menu-bar />
+            </v-sheet>
+          </v-col>
+
+          <v-col cols="12" sm="9">
+            <v-sheet min-height="70vh" rounded="lg">
+              <v-container>
+
+                <v-form ref="form">
+                  <p class="text-center">Enter the note you want to play<br><br></p>
+
+                  <v-text-field v-model="note" label="Note" variant="outlined"></v-text-field>
+
+                  <v-btn @click="playNote" block class="mt-2">Debug Stepper</v-btn><br><br>
+                </v-form>
+              </v-container>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
 
 <script>
 export default {
-    data: () => ({
-        links: [
-            "Dashboard",
-            "Play song (via Midi)",
-            "Play notes",
-            "Debug and calibrate motors",
-        ],
-        speed: 0,
-        channel: 0,
-        file: null,
-    }),
-    methods: {
-        sendAndPlay() {
-            const params = new URLSearchParams({
-                channel: this.channel,
-                speed: this.speed,
-                file: this.file,
-            });
-            const url = `http://192.168.1.1/play_midi?${params.toString()}`;
-            fetch(url, { method: "GET" })
-                .then(response => {
-                if (!response.ok) {
-                    throw new Error("Network response was not ok");
-                }
-                // handle successful response
-            })
-                .catch(error => {
-                console.error("There was a problem with the network request:", error);
-                // handle error
-            });
+  data: () => ({
+    note: null,
+    snackbarText: null,
+    snackbarColor: null,
+    snackbarVisible: false,
+  }),
+  methods: {
+    async playNote() {
+      try {
+        const response = await fetch(`http://192.168.1.1/play_note?note=${this.note}`);
+        if (response.ok) {
+          this.snackbarText = 'Note played successfully';
+          this.snackbarColor = 'success';
+        } else {
+          this.snackbarText = 'An error occurred while playing the note';
+          this.snackbarColor = 'error';
         }
-    }
+      } catch (error) {
+        this.snackbarText = 'An error occurred while playing the note';
+        this.snackbarColor = 'error';
+      } finally {
+        this.snackbarVisible = true;
+      }
+    },
+    hideSnackbar() {
+      this.snackbarVisible = false;
+    },
+  },
 }
 </script>
