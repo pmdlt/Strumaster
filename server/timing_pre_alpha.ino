78866d88dca5ed5eaaf1f8b4c15b31d559702ef8
Malen Raychev <malenraychev@gmail.com>
2023-05-23 12:55:15 +0200

    Better implementation,  id problem at first note

--- server/timing_pre_alpha.ino
+++ server/timing_pre_alpha.ino
@@ -1,4 +1,5 @@
 #include <CSV_Parser.h>
+#include <ArduinoQueue.h>
 
 float* duration;
 int* id;
@@ -9,22 +10,25 @@ int i = 0;
 
 unsigned long startTime;
 unsigned long currentTime;
+unsigned long lastActivatedTime = 0;  // Track the last activated time
+
+ArduinoQueue<int> timeQueue;  // Queue to store activation times
+
 
 void setup() {
   Serial.begin(9600);
 
-const char* csvData = "duration,id,time,velocity\n"
-                      "0.75,10,5.25,0.6929133858267716\n"
-                      "0.394874057291668,20,13.056699765625002,0.7165354330708661\n"
-                      "0.123456789,30,18.987654321,0.54321\n"
-                      "0.987654321,40,26.345678901,0.123456789\n"
-                      "0.54321,50,33.678901234,0.987654321\n"
-                      "0.246813575,60,41.345678901,0.7654321\n"
-                      "0.135791357,70,48.789012345,0.321987654\n"
-                      "0.987654321,80,56.876543210,0.987654321\n"
-                      "0.654321,90,64.567890123,0.432109876\n"
-                      "0.333333333,100,71.111111111,0.222222222\n";
-
+  const char* csvData = "duration,id,time,velocity\n"
+                        "0.75,10,5.25,0.6929133858267716\n"
+                        "0.394874057291668,20,13.056699765625002,0.7165354330708661\n"
+                        "0.123456789,30,18.987654321,0.54321\n"
+                        "0.987654321,40,26.345678901,0.123456789\n"
+                        "0.54321,50,33.678901234,0.987654321\n"
+                        "0.246813575,60,41.345678901,0.7654321\n"
+                        "0.135791357,70,48.789012345,0.321987654\n"
+                        "0.987654321,80,56.876543210,0.987654321\n"
+                        "0.654321,90,64.567890123,0.432109876\n"
+                        "0.333333333,100,71.111111111,0.222222222\n";
 
   CSV_Parser csvParser(csvData, "fLff");
 
@@ -48,27 +52,34 @@ const char* csvData = "duration,id,time,velocity\n"
     Serial.print(timeArray[i]);
     Serial.print(", velocity=");
     Serial.println(velocity[i]);
+    timeQueue.enqueue(timeArray[i] * 1000);
   }
 
-  startTime = millis(); // Start the timer
+  startTime = millis();
   Serial.println("Start_timer");
 }
 
 void loop() {
   if (i < rowCount) {
-    currentTime = millis() - startTime; // Calculate the elapsed time
+    currentTime = millis() - startTime;
 
-    if (currentTime >= timeArray[i] * 1000 && currentTime <= (timeArray[i] + duration[i]) * 1000) {
+    if (!timeQueue.isEmpty() && currentTime >= timeQueue.getHead() && currentTime > lastActivatedTime) {
       Serial.print("i: ");
       Serial.println(i);
       Serial.print("ID: ");
       Serial.print(id[i]);
       Serial.print(", Velocity: ");
       Serial.println(velocity[i]);
-    } else if (currentTime > (timeArray[i] + duration[i]) * 1000) {
+      lastActivatedTime = currentTime;
+      timeQueue.dequeue();
       i++;
+      //call stepper
+      //call servo
     }
+
+    // servo_id = id[i]/10
   }
 
   // Other code in the loop
 }
+
