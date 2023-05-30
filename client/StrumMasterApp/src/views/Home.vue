8166d0dca2dab781e72fdec97902aa6b6ea3193d
Paul Madelénat <18498650+pmdlt@users.noreply.github.com>
2023-05-30 14:55:22 +0000

    justify-center align-center

--- client/StrumMasterApp/src/views/Home.vue
+++ client/StrumMasterApp/src/views/Home.vue
@@ -1,20 +1,22 @@
 <template>
   <v-app id="inspire">
-    <v-main>
-      <v-container class="d-flex align-center justify-center">
+    <v-main class="bg-grey-lighten-3 justify-center align-center">
+      <v-container>
         <v-row>
           <v-col cols="12" sm="12">
             <v-sheet min-height="70vh" rounded="lg">
               <v-container>
-                <h1>Strumaster</h1>
+                <v-img contain :width="700" src="@/assets/logo.png" />
 
                 <p>Welcome to the control center app of the Strumaster project.<br>
+
                   You can control the guitar from this app, play a specific note and your favourite song via MIDI
                   file.<br><br>
                 </p>
 
                 <p>Please connect to the WiFi of the board (strumaster_control) with password
                   12345678.<br>
+
                   Then click the button below to test your connection with the board.<br><br>
                 </p>
                 <v-divider></v-divider><br>
@@ -34,7 +36,6 @@
     </v-main>
   </v-app>
 </template>
-
 
 <script>
 export default {
