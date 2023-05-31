dc8a89e482b2f59395c3aef7b1cd921096d252ef
Paul Madelénat <18498650+pmdlt@users.noreply.github.com>
2023-05-31 15:43:44 +0000

    Reorder

--- client/StrumMasterApp/src/views/Home.vue
+++ client/StrumMasterApp/src/views/Home.vue
@@ -2,29 +2,31 @@
   <v-app id="inspire">
     <v-main class="bg-grey-lighten-3">
       <v-container>
-        <v-sheet min-height="70vh" rounded="lg" class="d-flex justify-center">
-          <v-img contain :width="700" src="@/assets/logo.png" />
+        <v-sheet min-height="70vh" rounded="lg">
+          <v-container class="d-flex justify-center">
+            <v-img contain :width="700" src="@/assets/logo.png" />
 
-          <p>Welcome to the control center app of the Strumaster project.<br>
+            <p>Welcome to the control center app of the Strumaster project.<br>
 
-            You can control the guitar from this app, play a specific note and your favourite song via MIDI
-            file.<br><br>
-          </p>
+              You can control the guitar from this app, play a specific note and your favourite song via MIDI
+              file.<br><br>
+            </p>
 
-          <p>Please connect to the WiFi of the board (strumaster_control) with password
-            12345678.<br>
+            <p>Please connect to the WiFi of the board (strumaster_control) with password
+              12345678.<br>
 
-            Then click the button below to test your connection with the board.<br><br>
-          </p>
-          <v-divider></v-divider><br>
-          <p>Please remove strong HTTPS security for this website in your browser in order to
-            communicate with the board.<br>
-            Don't worry, security is not an issue here, we are not sending any sensitive data. Also, it's just
-            between your PC and the board, not you and the internet.<br>
-            <br>
-          </p>
-          <v-img contain :width="500" src="@/assets/1.png" />
-          <v-img contain :width="500" src="@/assets/2.png" />
+              Then click the button below to test your connection with the board.<br><br>
+            </p>
+            <v-divider></v-divider><br>
+            <p>Please remove strong HTTPS security for this website in your browser in order to
+              communicate with the board.<br>
+              Don't worry, security is not an issue here, we are not sending any sensitive data. Also, it's just
+              between your PC and the board, not you and the internet.<br>
+              <br>
+            </p>
+            <v-img contain :width="500" src="@/assets/1.png" />
+            <v-img contain :width="500" src="@/assets/2.png" />
+          </v-container>
         </v-sheet>
       </v-container>
     </v-main>
