51642611f36ff965ac8630f7243615bed76b7486
Paul Madelénat <18498650+pmdlt@users.noreply.github.com>
2023-05-31 15:56:50 +0000

    class="text-center"

--- client/StrumMasterApp/src/views/Home.vue
+++ client/StrumMasterApp/src/views/Home.vue
@@ -6,19 +6,19 @@
           <v-container class="d-flex flex-column justify-center align-center">
             <v-img contain :width="700" src="@/assets/logo.png" />
 
-            <p>Welcome to the control center app of the Strumaster project.<br>
+            <p class="text-center">Welcome to the control center app of the Strumaster project.<br>
 
               You can control the guitar from this app, play a specific note and your favourite song via MIDI
               file.<br><br>
             </p>
 
-            <p>Please connect to the WiFi of the board (strumaster_control) with password
+            <p class="text-center">Please connect to the WiFi of the board (strumaster_control) with password
               12345678.<br>
 
               Then click the button below to test your connection with the board.<br><br>
             </p>
             <v-divider></v-divider><br>
-            <p>Please remove strong HTTPS security for this website in your browser in order to
+            <p class="text-center">Please remove strong HTTPS security for this website in your browser in order to
               communicate with the board.<br>
               Don't worry, security is not an issue here, we are not sending any sensitive data. Also, it's just
               between your PC and the board, not you and the internet.<br>
